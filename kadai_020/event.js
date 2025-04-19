aed48697610f7c285504d27477c2e6fb38093b78
bluealice56 <149145074+bluealice56@users.noreply.github.com>
2025-04-19 17:22:15 +0900

    課題20

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,8 +1,6 @@
-//HTML要素を取得
-const jsBtn = document.getElementById('btn');
+const btn = document.getElementById("btn");
+const text = document.getElementById("text");
 
-//要素にイベントのとき、変化つける
-jsBtn.addEventListener('click', () => {
-    const newText = document.getElementById('text');
-    newText.textContent="ボタンをクリックしました";
-  });
+btn.addEventListener("click", () => {
+  text.textContent = "ボタンをクリックしました";
+});
